d13e01e3ae81df4132d3f9b5c32d63113fb6708c
Diego Calvo <100451091@alumnos.uc3m.es>
2023-06-29 11:25:48 +0200

    improvement(ui): Simplify main module and move change page handler to ui module

--- src/public/main.js
+++ src/public/main.js
@@ -1,53 +1,14 @@
 import { loadExerciseSets } from "./sockets.js";
-import { renderExerciseSets, onHandleSubmitSetForm } from "./ui.js";
+import { renderExerciseSets, onHandleSubmitSetForm, changeMainPage } from "./ui.js";
 import { ExercisesList } from './exercisesList.js'
 
 loadExerciseSets(renderExerciseSets);
 
 const setForm = document.querySelector("#new-set-form");
 setForm.addEventListener("submit", onHandleSubmitSetForm);
-
-const PAGE_TYPES = Object.freeze({
-  NEW_DAY: "new-day",
-  TRAINING_SETS: "training-sets",
-  PROGRESS: "progress",
-  SETTINGS: "settings",
+document.querySelectorAll(".mobile-nav__item").forEach((item) => {
+  item.addEventListener("pointerup", (event) => changeMainPage(event.target.dataset.page));
 });
 
-let current_page = changePage(PAGE_TYPES.NEW_DAY, PAGE_TYPES.TRAINING_SETS);
-
-function changePage(current_page, new_page) {
-  // Check if the new page is in the valid types
-  if (
-    current_page !== new_page &&
-    Object.values(PAGE_TYPES).includes(new_page)
-  ) {
-    console.log("New page: ", new_page);
-    // Update the nav item
-    document
-      .querySelector(`[data-page="${current_page}"]`)
-      .classList.remove("active-nav-item");
-    document
-      .querySelector(`[data-page="${new_page}"]`)
-      .classList.add("active-nav-item");
-
-    // Show only the current page
-    Object.values(PAGE_TYPES).forEach(page => {
-      document.querySelector(`#page_${page}`).style.display = 'none';
-    })
-    document.querySelector(`#page_${new_page}`).style.display = 'flex';
-
-    return new_page;
-  }
-  return current_page;
-}
-
-window.addEventListener("load", () => {
-  document.querySelectorAll(".mobile-nav__item").forEach((item) => {
-    item.addEventListener("pointerup", (event) => {
-      current_page = changePage(current_page, event.target.dataset.page);
-    });
-  });
-  const exerciseList = new ExercisesList(document.getElementById("exercises-list"));
-  exerciseList.render();
-});
+const exerciseList = new ExercisesList(document.getElementById("exercises-list"));
+exerciseList.render();
--- src/public/ui.js
+++ src/public/ui.js
@@ -1,6 +1,13 @@
 import { saveExerciseSet } from "./sockets.js";
 
+const PAGE_TYPES = Object.freeze({
+  NEW_DAY: "new-day",
+  TRAINING_SETS: "training-sets",
+  PROGRESS: "progress",
+  SETTINGS: "settings",
+});
 const setLists = document.querySelector("#sets-list");
+let currentPage = "training-sets";
 
 /** Return the UI element of an exercise set
  * @param {object} exerciseSet
@@ -16,10 +23,14 @@ const exerciseSetUI = (exerciseSet) => {
   setItem.innerHTML = `
     <h3 class="set-item__title">${exerciseSet.name}</h3>
     <p class="set-item__description">${exerciseSet.description}</p>
-    ${exerciseSet.exercises ? `<button class="set-item__expand">Expand</button>` : ""}
+    ${
+      exerciseSet.exercises
+        ? `<button class="set-item__expand">Expand</button>`
+        : ""
+    }
     <button class="set-item__delete">Delete</button>
   `;
-  
+
   return setItem;
 };
 
@@ -41,4 +52,35 @@ export const onHandleSubmitSetForm = (event) => {
     event.target["description"].value,
     []
   );
+};
+
+/** Change the main page to the new page
+ * @param   {string}  new_page  The new page that will be shown
+ */
+export const changeMainPage = (new_page) => {
+  // Check if the new page is in the valid types
+  if (
+    currentPage !== new_page &&
+    Object.values(PAGE_TYPES).includes(new_page)
+  ) {
+    console.log("New page: ", new_page);
+    // Update the nav item
+    document
+      .querySelector(`[data-page="${currentPage}"]`)
+      .classList.remove("active-nav-item");
+    document
+      .querySelector(`[data-page="${new_page}"]`)
+      .classList.add("active-nav-item");
+
+    // Show only the current page
+    document
+      .querySelector(`#page_${currentPage}`)
+      .classList.remove("active-main-container");
+    document
+      .querySelector(`#page_${new_page}`)
+      .classList.add("active-main-container");
+
+    // Update the current page
+    currentPage = new_page;
+  }
 };
