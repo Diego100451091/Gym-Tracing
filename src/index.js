89d5a2fe52a79051e1125e473038ba7ba164bbe9
Diego Calvo <100451091@alumnos.uc3m.es>
2023-06-29 10:21:51 +0200

    Client app updated with the exercise set creator implementations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app";
 import { Server as WebsocketServer } from "socket.io";
 import http from "http";
+import sockets from './sockets'
 
 import {connectDB} from './db';
 
@@ -8,53 +9,7 @@ connectDB();
 
 const server = http.createServer(app);
 const httpServer = server.listen(3000);
-const io = new WebsocketServer(server);
-
 console.log('Server is runnign in port 3000');
 
-// const http = require('http');
-// const socketIO = require('socket.io');
-// const fs = require('fs');
-
-// const server = http.createServer(app);
-// const io = socketIO(server);
-
-// const PORT = 3000;
-
-// app.use(express.static('src/public'));
-
-// // Ruta para obtener los datos del archivo JSON
-// app.get('/data', (req, res) => {
-//   fs.readFile('data.json', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return res.status(500).send('Error al obtener los datos.');
-//     }
-//     res.json(JSON.parse(data));
-//   });
-// });
-
-// // Escucha las conexiones de Socket.IO
-// io.on('connection', (socket) => {
-//   console.log('Nueva conexión de socket:', socket.id);
-
-//   socket.emit("messages", {
-//     id: 1,
-//     text: `Tu id es ${socket.id}`
-//   })
-//   // // Lógica para manipular los datos y actualizar el archivo JSON
-
-//   // // Ejemplo: Escuchar el evento 'guardarDatos' desde el cliente
-//   // socket.on('guardarDatos', (datos) => {
-//   //   // Actualizar los datos en el archivo JSON o realizar otras operaciones
-//   //   // ...
-//   //   console.log(datos);
-
-//   //   // Enviar una respuesta al cliente
-//   //   socket.emit('datosGuardados', { mensaje: 'Datos guardados exitosamente' });
-//   // });
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`Servidor iniciado en el puerto ${PORT}`);
-// });
+const io = new WebsocketServer(server);
+sockets(io);
--- src/public/main.js
+++ src/public/main.js
@@ -1,39 +1,53 @@
-const socket = io.connect("http://localhost:3000/", {'forceNew': true});
+import { loadExerciseSets } from "./sockets.js";
+import { renderExerciseSets, onHandleSubmitSetForm } from "./ui.js";
+import { ExercisesList } from './exercisesList.js'
 
-socket.on("messages", function(data) {
-    console.log(data);
-})
+loadExerciseSets(renderExerciseSets);
 
+const setForm = document.querySelector("#new-set-form");
+setForm.addEventListener("submit", onHandleSubmitSetForm);
 
 const PAGE_TYPES = Object.freeze({
-    NEW_DAY: "new-day",
-    TRAINING_SETS: "training-sets",
-    PROGRESS: "progress",
-    SETTINGS: "settings"
-})
+  NEW_DAY: "new-day",
+  TRAINING_SETS: "training-sets",
+  PROGRESS: "progress",
+  SETTINGS: "settings",
+});
 
-let current_page = PAGE_TYPES.NEW_DAY;
+let current_page = changePage(PAGE_TYPES.NEW_DAY, PAGE_TYPES.TRAINING_SETS);
 
-function changePage (new_page){
-    // Check if the new page is in the valid types
-    if (current_page !== new_page && Object.values(PAGE_TYPES).includes(new_page)){
-        console.log("New page: ", new_page)
-        // Update the nav item
-        document.querySelector(`[data-page="${current_page}"]`).classList.remove("active-nav-item");
-        document.querySelector(`[data-page="${new_page}"]`).classList.add("active-nav-item");
+function changePage(current_page, new_page) {
+  // Check if the new page is in the valid types
+  if (
+    current_page !== new_page &&
+    Object.values(PAGE_TYPES).includes(new_page)
+  ) {
+    console.log("New page: ", new_page);
+    // Update the nav item
+    document
+      .querySelector(`[data-page="${current_page}"]`)
+      .classList.remove("active-nav-item");
+    document
+      .querySelector(`[data-page="${new_page}"]`)
+      .classList.add("active-nav-item");
 
-        // // Hide the current page
-        // document.getElementById(`page_${current_page}`).style.display = "none";
-        // // Show the new page
-        // document.getElementById(`page_${new_page}`).style.display = "flex";
+    // Show only the current page
+    Object.values(PAGE_TYPES).forEach(page => {
+      document.querySelector(`#page_${page}`).style.display = 'none';
+    })
+    document.querySelector(`#page_${new_page}`).style.display = 'flex';
 
-        // Update the current page
-        current_page = new_page;
-    }
+    return new_page;
+  }
+  return current_page;
 }
 
-window.addEventListener("load", () => document.querySelectorAll(".mobile-nav__item").forEach((item) => {
+window.addEventListener("load", () => {
+  document.querySelectorAll(".mobile-nav__item").forEach((item) => {
     item.addEventListener("pointerup", (event) => {
-        changePage(event.target.dataset.page);
-    })
-}))
+      current_page = changePage(current_page, event.target.dataset.page);
+    });
+  });
+  const exerciseList = new ExercisesList(document.getElementById("exercises-list"));
+  exerciseList.render();
+});
