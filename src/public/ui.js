0df4edf472a49889892d1998818e23ef884e2cef
Diego Calvo <100451091@alumnos.uc3m.es>
2023-06-29 16:44:28 +0200

    feat(training set page): Added the animation and funtionality to change between 'create set' and 'training sets' view

--- src/public/ui.js
+++ src/public/ui.js
@@ -6,8 +6,14 @@ const PAGE_TYPES = Object.freeze({
   PROGRESS: "progress",
   SETTINGS: "settings",
 });
+let currentPage = PAGE_TYPES.TRAINING_SETS;
 const setLists = document.querySelector("#sets-list");
-let currentPage = "training-sets";
+const trainingSetContainer = document.querySelector("#training-set__container");
+const [creatorNavItem, setsNavItem] = document.querySelectorAll(".training-set__nav-item");
+
+// Listeners for events
+creatorNavItem.addEventListener("click", () => setViewCreateSet());
+setsNavItem.addEventListener("click", () => setViewTrainingSets());
 
 /** Return the UI element of an exercise set
  * @param {object} exerciseSet
@@ -33,6 +39,107 @@ const exerciseSetUI = (exerciseSet) => {
 
   return setItem;
 };
+
+/** Translate the training sets page container when the user move the pointer over it
+ * @param {TouchEvent} event
+ */
+const onMoveTrainingSetContainer = (event) => {
+  const startX = trainingSetContainer.dataset.startMove
+    ? trainingSetContainer.dataset.startMove
+    : 0;
+  const currentX = event.touches[0].clientX;
+
+  if (startX == 0) {
+    trainingSetContainer.dataset.startMove = currentX;
+  } else {
+    const offset = currentX - startX;
+    let translation = 0;
+    // Check if the training set container is already translated
+    if (trainingSetContainer.dataset.isTranslated === "true") {
+      // Add the screen width to the translation for save the current translation
+      translation = -(screen.width - offset);
+    } else {
+      // The translation will be the actual offset, because the current translation is 0
+      translation = offset;
+    }
+
+    // Move the container between the screen zone
+    if (translation > 0) {
+      trainingSetContainer.style.transform = `translate(0px)`;
+    } else if (translation < -screen.width) {
+      trainingSetContainer.style.transform = `translate(-${screen.width}px)`;
+    } else {
+      trainingSetContainer.style.transform = `translate(${translation}px)`;
+    }
+  }
+};
+
+/** Manage the end of the touch event over the training set container*/
+const onTouchEndTrainingSetContainer = () => {
+  // Get the element of the container, to know its position after the translation
+  const divElement = trainingSetContainer.getBoundingClientRect();
+  // Get the position of the middle of the container
+  const middlePosition = divElement.left + divElement.width / 2;
+  if (middlePosition < screen.width / 2) {
+    // If the middle position is less than the center, set the training set view
+    setViewTrainingSets();
+  } else {
+    // If the middle position is greater than the center, set the create set view
+    setViewCreateSet();
+  }
+  // Reset the values of the container
+  trainingSetContainer.dataset.startMove = 0;
+  trainingSetContainer.dataset.offset = 0;
+};
+
+/** Set the 'create set' view as the visible one*/
+const setViewCreateSet = () => {
+  // Change the nav items style
+  creatorNavItem.classList.add("active-training-set__nav-item");
+  setsNavItem.classList.remove("active-training-set__nav-item");
+  // Move the container set to show the properly view
+  trainingSetContainer.style.transform = `translate(0)`;
+  trainingSetContainer.dataset.isTranslated = "false";
+};
+
+/** Set the 'training sets' view as the visible one, and change the nav style */
+const setViewTrainingSets = () => {
+  // Change the nav items style
+  creatorNavItem.classList.remove("active-training-set__nav-item");
+  setsNavItem.classList.add("active-training-set__nav-item");
+  // Move the container set to show the properly view
+  trainingSetContainer.style.transform = `translate(-${screen.width}px)`;
+  trainingSetContainer.dataset.isTranslated = "true";
+};
+
+document.body.addEventListener("touchmove", function (event) {
+  // Search if the element that has been moved is into the container
+  let parent = event.target;
+  while (parent.tagName != "BODY") {
+    if (parent.id == "training-set__container") {
+      // Execute the event handler
+      onMoveTrainingSetContainer(event);
+      break;
+    }
+    // Get the parent node until the 'body'
+    parent = parent.parentNode;
+  }
+});
+
+document.body.addEventListener("touchend", function (event) {
+  // Search if the element that has been 'touchend' is into the container
+  let parent = event.target;
+  while (parent.tagName != "BODY") {
+    if (parent.id == "training-set__container") {
+      // Execute the event handler
+      onTouchEndTrainingSetContainer();
+      break;
+    }
+    // Get the parent node until the 'body'
+    parent = parent.parentNode;
+  }
+});
+
 
 /** Render the exercise sets into the list of sets element
  * @param {object[]} sets
