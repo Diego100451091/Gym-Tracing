fb5bdf184670ff679aa8e2172a61938e417dadf6
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-05 16:03:55 +0200

    style(exercise-card): finished first version of exercise cards

--- src/public/exercisesList.js
+++ src/public/exercisesList.js
@@ -14,33 +14,61 @@ export class ExercisesList {
 
   _getExerciseUI = (exercise) => {
     const exUI = document.createElement("li");
-    exUI.classList.add("exercise-list__item");
+    exUI.classList.add("exercise-card");
     exUI.innerHTML = `
-            <div class="exercise-list__item__image">
+          <div class="main-content">
+            <div class="image">
                 <img src="${exercise.imagen}" alt="${exercise.nombre}">
             </div>
-            <div class="exercise-list__item__info">
-                <h3 class="exercise-list__item__info__title">${exercise.nombre}</h3>
-                <p class="exercise-list__item__info__muscle">Main muscle: ${exercise.musculo}</p>
-                <p class="exercise-list__item__info__description">${exercise.descripcion}</p>
-
+            <div class="information">
+                <h3 class="title">${exercise.nombre}</h3>
+                <p class="muscle">Main muscle: ${exercise.musculo}</p>
+                <p class="category">Category: ${exercise.categoria}</p>
             </div>
-            <input type="checkbox" class="exercise-list__checkbox" data-id="${exercise.id}">
+            <input type="checkbox" class="checkbox" data-id="${exercise.id}">
+          </div>
+          <button class="description-button" data-active="false">
+            Show description
+            <i class="fas fa-chevron-down"></i>  
+          </button>
+          <p class="description">
+            ${exercise.descripcion}
+          </p>
         `;
 
-    const checkbox = exUI.querySelector(".exercise-list__checkbox");
+    const descriptionButton = exUI.querySelector(".description-button");
+    descriptionButton.addEventListener("click", () => {
+      const description = exUI.querySelector(
+        ".description"
+      );
+      description.classList.toggle("visible");
+      if (descriptionButton.dataset.active === "false") {
+        descriptionButton.dataset.active = "true";
+        descriptionButton.innerHTML =
+          "Hide description <i class='fas fa-chevron-up'></i>";
+      } else {
+        descriptionButton.dataset.active = "false";
+        descriptionButton.innerHTML =
+          "Show description <i class='fas fa-chevron-down'></i>";
+      }
+    });
+
+    const checkbox = exUI.querySelector(".checkbox");
     checkbox.addEventListener("click", () =>
-      this._onClickExerciseCheckbox(checkbox)
+      this._onClickExerciseCheckbox(exUI, checkbox)
     );
+
     return exUI;
   };
 
-  _onClickExerciseCheckbox = (checkboxEl) => {
+  _onClickExerciseCheckbox = (exEl, checkboxEl) => {
     if (checkboxEl.checked) {
       this.selectedExercises.push(checkboxEl.dataset.id);
+      exEl.classList.add("active")
     } else {
       const index = this.selectedExercises.indexOf(checkboxEl.dataset.id);
       this.selectedExercises.splice(index, 1);
+      exEl.classList.remove("active")
     }
     console.log(this.selectedExercises);
   };
