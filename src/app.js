b6d51f53cdc5b126187e7a19e58028650e33817c
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-13 23:24:57 +0200

    module: installed 'cookie-parser' module
    Module for parse the cookies received in api request as jsons

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import morgan from "morgan";
-import authRoutes from './routes/auth.routes.js';
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 // Show in console the request type and the route
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Able convert API request body into JSON format
 app.use(express.json());
+// Able to read cookies as json in API request
+app.use(cookieParser());
 // Set the routes for the API
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
 export default app;
