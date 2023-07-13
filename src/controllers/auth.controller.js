83ba3102bacde6584e800b3761e1dd9afdb050e6
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-13 16:15:56 +0200

    feat(register api): implemented password encryption when the user is created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,29 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  console.log(username, email, password);
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
   const newUser = new User({
     username,
     email,
-    password,
+      password: passwordHash,
   });
 
-  try {
     const userSaved = await newUser.save();
     console.log(`USER REGISTER SUCCESS: New user '${email}'`);
-    res.json(userSaved);
+    res.json({
+      id: userSaved.id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved._createdAt,
+    });
   } catch (error) {
     console.log("USER REGISTER ERROR:", error);
+    res.status(500).json({ message: error.message });
   }
 
 };
