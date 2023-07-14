d68842cc31767d4fb23a803a63463d676a1bed77
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-14 23:02:16 +0200

    feat(app): added routing component to change between pages content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from './components/Header.jsx';
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1 className="">Home page</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/sets" element={<h1>Sets</h1>} />
+          <Route path="/progress" element={<h1>Progress</h1>} />
+          <Route path="/settings" element={<h1>Settings</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
