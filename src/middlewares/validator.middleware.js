ecbf56e1f3da3e32086beec44f1187b08cb82ba2
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-14 16:19:44 +0200

    feat(auth api): implemented validation in request data
    Implemented 'zod' schemes to validate data provided by the front-end before using it in authentication methods

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,12 @@
+export const validateSchema = (schema, value) => (req, res, next) => {
+  try {
+    if (value){
+        schema.parse(value);
+    } else {
+        schema.parse(req.body);
+    }
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: error.errors.map(error => error.message) });
+  }
+};
