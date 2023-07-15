e68ea023cff61e31c7a3d5ab843fd5374cf62ba9
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-15 10:16:13 +0200

    style(header): changed header style with his own size

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo-white.png"
 
 export function Header () {
   return (
-    <header className="bg-primary-light font-bold w-full flex justify-center items-center relative z-100">
+    <header className="bg-primary-light font-bold w-full h-12 fixed top-0 left-0  flex justify-center items-center z-100">
       <img className="h-[60%] aspect-square" src={logo} alt="Logo" />
     </header>
   );
