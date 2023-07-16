a43c7ba05aec5a24ee4a0e111652df9e1dbcba6f
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-16 20:14:56 +0200

    resources(logos): converted png logos into svg format

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from "../assets/logo-white.png"
+import logo from "../assets/logo-white.svg"
 
 export function Header () {
   return (
-    <header className="bg-primary-light font-bold w-full h-12 fixed top-0 left-0  flex justify-center items-center z-100">
+    <header className="bg-primary-light font-bold w-full h-12 fixed top-0 left-0 flex justify-center items-center z-50">
       <img className="h-[60%] aspect-square" src={logo} alt="Logo" />
     </header>
   );
