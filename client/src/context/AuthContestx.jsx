a540e293e5274ff26bedeff6ba8d2a9da527a841
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-16 23:40:35 +0200

    feat(auth context): added a state to represent if a user is already authenticated

--- client/src/context/AuthContestx.jsx
+++ client/src/context/AuthContestx.jsx
@@ -4,26 +4,33 @@ import { registerRequest } from "../api/auth.api.js";
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error("useAuth must be used within an AuthProvider");
-    }
-    return context;
-}
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const signup = async (user) => {
-    const response = await registerRequest(user);
-    console.log(response.data);
-    setUser(response.data);
+    try {
+      const response = await registerRequest(user);
+      console.log(response.data);
+      setUser(response.data);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.log("Signup error:", error);
+    }
   };
 
   return (
     <AuthContext.Provider
       value={{
         user,
+        isAuthenticated,
         signup,
       }}
     >
