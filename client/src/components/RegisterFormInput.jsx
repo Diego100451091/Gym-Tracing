46e8348894338db7c5c57d484d11b8dad0597f6b
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-16 20:35:49 +0200

    feat(component): implemented register form input
    Component that represent each input in register page

--- client/src/components/RegisterFormInput.jsx
+++ client/src/components/RegisterFormInput.jsx
@@ -1,12 +1,51 @@
-function RegisterFormInput({ label, type, required, registerHandler }) {
+import { useState, useEffect } from "react";
+
+function RegisterFormInput({
+  id,
+  label,
+  type,
+  size,
+  registerHandler,
+  placeholder,
+  requirements,
+  errorsHandler,
+  isUsed,
+}) {
+  const [status, setStatus] = useState("neutral");
+
+  const updateStatus = () => {
+    if (errorsHandler) {
+      setStatus("error");
+    } else {
+      if (!isUsed) {
+        setStatus("neutral");
+      } else {
+        setStatus("success");
+      }
+    }
+  }
+
+  useEffect(updateStatus, [errorsHandler]);
+
   return (
-    <div className="relative w-full ">
-      <label htmlFor={label} className="absolute bg-white text-primary-light top-1 left-2.5 pl-1 pr-1">{label}</label>
+    <div className={`relative ${size}`}>
+      <label
+        htmlFor={label}
+        className={`absolute bg-white text-sm top-1 left-2.5 pl-1 pr-1 text-${status}`}
+      >
+        {label}
+      </label>
       <input
         type={type}
-        {...registerHandler(label, { required: required })}
-        className="w-full h-auto mt-4 outline outline-2 outline-primary-light rounded-lg p-2"
+        {...registerHandler(id, requirements)}
+        className={`w-full h-auto mt-4 outline outline-2 rounded-lg px-4 py-3 text-sm outline-${status}`}
+        placeholder={placeholder}
       />
+      {errorsHandler && (
+        <p className="text-error-light px-2.5 text-justify pt-1 text-xs">
+          ✖ {errorsHandler.message}
+        </p>
+      )}
     </div>
   );
 }
