0c6197ff2cd8192a6505a96ce42289b359800632
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-16 22:39:23 +0200

    feat(register page): finish register page style and added input value verifications

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,50 +1,129 @@
 import { useForm } from "react-hook-form";
-import { Header } from "../components/Header.jsx";
 import RegisterFormInput from "../components/RegisterFormInput.jsx";
+import { registerSchema } from "../schemas/auth.shemas.js";
+import { zodResolver } from "@hookform/resolvers/zod";
+import backgroundPattern from '../assets/background-pattern.webp';
+import orangeLogo from "../assets/logo-orange.svg"
 
 function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, dirtyFields },
+  } = useForm({ resolver: zodResolver(registerSchema) });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
 
   return (
-    <div className="h-full min-h-full relative">
-      <Header />
-      <div className="w-full h-full pb-12 flex flex-col justify-center items-center bg-primary-light">
-        <h1 className="text-4xl font-bold text-white">Register</h1>
-        <form
-          action=""
-          onSubmit={handleSubmit((values) => {
-            console.log(values);
-          })}
-          className="max-w-500px w-9/12 flex flex-col gap-4 bg-white rounded-xl p-5"
-        >
-          <RegisterFormInput
-            label="Username"
-            type="text"
-            required={true}
-            registerHandler={register}
-          />
-          <RegisterFormInput
-            label="Email"
-            type="email"
-            required={true}
-            registerHandler={register}
-          />
-          <RegisterFormInput
-            label="Password"
-            type="password"
-            required={true}
-            registerHandler={register}
-          />
-          <RegisterFormInput
-            label="Confirm Password"
-            type="password"
-            required={true}
-            registerHandler={register}
-          />
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    </div>
+    <section className="lg:grid lg:min-h-screen lg:grid-cols-12 bg-white ">
+      <aside className="bg-primary-light relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+        <img
+          alt="Pattern"
+          src={backgroundPattern}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </aside>
+
+      <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+        <div className="max-w-xl lg:max-w-3xl">
+          
+          <a className="bloc" href="/">
+            <span className="sr-only">Home</span>
+            <img src={orangeLogo} className="h-8 sm:h-10"></img>
+          </a>
+            
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black m-0 mt-6 ">
+            Wellcome to Gym tracking!
+          </h1>
+          <p className="mt-4 leading-relaxed text-justify text-slate-500">
+            Start tracking your fitness journey with us and reach your goals
+            faster. Join now for personalized workouts and analyze your
+            progress. Let's get started!
+          </p>
+          <form
+            action=""
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-8 grid grid-cols-6 gap-6"
+          >
+            <RegisterFormInput
+              id="username"
+              label="Username"
+              type="text"
+              size="col-span-6"
+              registerHandler={register}
+              placeholder="Ej.: Charles17"
+              errorsHandler={errors.username}
+              isUsed={dirtyFields.username}
+            />
+
+            <RegisterFormInput
+              id="email"
+              label="Email"
+              type="email"
+              size="col-span-6"
+              registerHandler={register}
+              placeholder="Ej.: charles@gmail.com"
+              errorsHandler={errors.email}
+              isUsed={dirtyFields.email}
+            />
+
+            <RegisterFormInput
+              id="password"
+              label="Password"
+              type="password"
+              size="col-span-6 sm:col-span-3"
+              registerHandler={register}
+              placeholder="******"
+              errorsHandler={errors.password}
+              isUsed={dirtyFields.password}
+            />
+
+            <RegisterFormInput
+              id="passwordConfirmation"
+              label="Password confirmation"
+              type="password"
+              size="col-span-6 sm:col-span-3"
+              registerHandler={register}
+              placeholder="******"
+              errorsHandler={errors.passwordConfirmation}
+              isUsed={dirtyFields.passwordConfirmation}
+            />
+
+            <p className="col-span-6 text-sm text-gray-500">
+              By creating an account, you agree to our
+              {" "}
+              <a
+                href="/privacy-policy"
+                className="text-sm text-gray-700 underline"
+              >
+                privacy policy
+              </a>
+              .
+            </p>
+
+            <div className="col-span-6 flex items-center justify-center gap-4 flex-wrap">
+              <button
+                type="submit"
+                className="medium-button bg-primary-light text-white w-max px-8"
+              >
+                Create an account
+              </button>
+
+              <p className="text-sm text-gray-500">
+                Already have an account?
+                {" "}
+                <a href="/login" className="text-sm text-primary-light underline">
+                  Log in
+                </a>
+                .
+              </p>
+            </div>
+          </form>
+        </div>
+      </main>
+    </section>
   );
 }
 
