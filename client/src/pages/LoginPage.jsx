09b00d3539b5a328360564038c3c482a95ddb987
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-17 13:12:48 +0200

    feat(login page): implemented login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,45 @@
-import { Header } from "../components/Header.jsx";
+import LoginForm from "../components/LoginForm.jsx";
+
+import orangeLogo from '../assets/logo-orange.svg';
+import backgroundPattern from "../assets/background-pattern.webp";
 
 function LoginPage() {
   return (
-    <>
-      <Header />
-      <div>
-        <h1>Login Page</h1>
-      </div>
-    </>
+    <section className="lg:grid lg:min-h-screen lg:grid-cols-12 bg-white ">
+      <aside className="bg-primary-light relative block h-16 lg:col-span-5 lg:h-full xl:col-span-6">
+        <img
+          alt="Pattern"
+          src={backgroundPattern}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </aside>
+      <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-6 xl:col-span-6">
+        <div className="max-w-xl lg:max-w-3xl lg:shadow-xl lg:p-10 lg:rounded-xl">
+          <a className="" href="/">
+            <span className="sr-only">Home</span>
+            <img src={orangeLogo} className="h-8"></img>
+          </a>
+          <h1 className="text-2xl sm:text-3xl md:text-3xl font-bold text-black m-0 mt-4">
+            Login
+          </h1>
+          <p className="mt-2 text-sm leading-normal text-justify text-slate-500">
+            Please, enter your email and password to login.
+          </p>
+          <LoginForm>
+            <p className="text-sm text-gray-500">
+              Don't you have an account yet?{" "}
+              <a
+                href="/register"
+                className="text-sm text-primary-light underline"
+              >
+                Register
+              </a>
+              .
+            </p>
+          </LoginForm>
+        </div>
+      </main>
+    </section>
   );
 }
 
