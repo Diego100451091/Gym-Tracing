e4b9ad1bd31c99d45b884cd080e6a0562645b32d
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-17 10:58:40 +0200

    fix: app import sintax error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContestx.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import RegisterPage from './pages/RegisterPage.jsx';
 import LoginPage from "./pages/LoginPage.jsx";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage.jsx";
