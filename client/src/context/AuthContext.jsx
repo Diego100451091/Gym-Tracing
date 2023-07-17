e81e2b6b65a7a53c49dc67148b5a399383c13a5e
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-17 19:58:48 +0200

    feat(auth context): verify the user token
    When the component is mounted, and manage 'isLoading' status to know when the verification is ready

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,10 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { registerRequest, loginRequest } from "../api/auth.api.js";
+import {
+  registerRequest,
+  loginRequest,
+  verifyTokenRequest,
+} from "../api/auth.api.js";
+import Cookies from "js-cookie";
 
 export const AuthContext = createContext();
 
@@ -15,35 +20,67 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const saveUser = (data) => {
+    setUser(data);
+    setIsAuthenticated(true);
+    setIsLoading(false);
+  }
+
+  const resetUser = () => {
+    setUser(null);
+    setIsAuthenticated(false);
+    setIsLoading(false);
+  }
 
   const signup = async (user) => {
     try {
       const response = await registerRequest(user);
-      setUser(response.data);
-      setIsAuthenticated(true);
+      saveUser(response.data)
     } catch (error) {
       setError(error.response.data);
+      resetUser();
     }
   };
 
   const login = async (user) => {
     try {
       const response = await loginRequest(user);
-      setUser(response.data);
-      setIsAuthenticated(true);
+      saveUser(response.data);
     } catch (error) {
       setError(error.response.data);
+      resetUser();
     }
   };
 
   useEffect(() => {
-    if(error.length > 0) {
+    if (error.length > 0) {
       const timer = setTimeout(() => {
-        setError([])
-      }, 3000)
+        setError([]);
+      }, 3000);
       return () => clearTimeout(timer);
     }
-  }, [error])
+  }, [error]);
+
+  useEffect(() => {
+    async function verifyToken() {
+      const cookies = Cookies.get();
+      if (!cookies.token) return resetUser();
+
+      try {
+        const response = await verifyTokenRequest();
+        if (!response.data) return resetUser();
+        
+        console.log(response.data);
+        return saveUser(response.data);
+      } catch (error) {
+        console.log(error);
+        return resetUser();
+      }
+    }
+    verifyToken();
+  }, []);
 
   return (
     <AuthContext.Provider
@@ -53,6 +90,7 @@ export const AuthProvider = ({ children }) => {
         error,
         signup,
         login,
+        isLoading
       }}
     >
       {children}
