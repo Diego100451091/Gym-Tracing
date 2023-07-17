8016e40c20b4fe446b7c7d5469e14cbb66b6b56d
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-17 13:01:58 +0200

    feat(auth schema): created login schema to validate login form inputs

--- client/src/schemas/auth.shemas.js
+++ client/src/schemas/auth.shemas.js
@@ -41,3 +41,21 @@ export const registerSchema = z
     message: "Passwords don't match",
     path: ["passwordConfirmation"],
   });
+
+export const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Email is not valid",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number"),
+});
