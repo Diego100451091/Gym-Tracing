d4390c27badbb9838d730ec7db51e428c2afa799
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-17 13:12:03 +0200

    feat(login hook): created custom hook to manaeg the login authentication logic

--- client/src/hooks/auth.hooks.js
+++ client/src/hooks/auth.hooks.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
-import { thowSimpleError } from "../alerts/AlertProvider.jsx";
+import { throwSimpleError, throwTemporalError } from "../alerts/AlertProvider.jsx";
 
 /**
  * Custom hook that handles the authentication logic for the sign in form
@@ -17,8 +17,24 @@ export const useSignAuthentication = () => {
 
   useEffect(() => {
     if (registerError.length > 0)
-      thowSimpleError("Register errors", registerError.join("<br>"));
+      throwSimpleError("Register errors", registerError.join("<br>"));
   }, [registerError]);
 
   return { signup };
 };
+
+export const useLoginAuthentication = () => {
+  const { login, isAuthenticated, error: loginError } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/workouts");
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (loginError.length > 0)
+      throwTemporalError(loginError.join("<br>"));
+  }, [loginError]);
+
+  return { login };
+}
