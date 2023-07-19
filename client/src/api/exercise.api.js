1f54f2290a52b3463eae28ae5cf981b9ebe3c1c1
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-19 21:48:03 +0200

    feat(exercises api): implemented client api methods for request exercise data

---
+++ client/src/api/exercise.api.js
@@ -0,0 +1,22 @@
+import axios from "./axios.js";
+
+export const requestExercises = () => axios.get("/exercises");
+
+export const requestBodyPartList = () =>
+  axios.get("/exercises/bodyParts");
+
+export const requestTargetMuscleList = () =>
+  axios.get("/exercises/targetMuscles");
+
+export const requestEquipementList = () =>
+  axios.get("/exercises/equipments");
+
+export const makeApiRequest = async (apiRequest) => {
+  try {
+    const response = await apiRequest();
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
