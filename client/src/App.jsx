fae0c038dbd544a6f3db71538957cf2be0ab4283
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-19 22:08:11 +0200

    feat (context providers): added exercise and workout providers to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ExerciseProvider } from "./context/ExerciseContext.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage.jsx";
@@ -8,26 +9,31 @@ import WorkoutsPage from "./pages/WorkoutsPage.jsx";
 import ProgressPage from "./pages/ProgressPage.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";
 import WorkoutFormPage from "./pages/WorkoutFormPage.jsx";
-import ProtectedRoutes from './ProtectedRoutes.jsx';
+import ProtectedRoutes from "./ProtectedRoutes.jsx";
+import { WorkoutProvider } from "./context/WorkoutContext.jsx";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+      <ExerciseProvider>
+        <WorkoutProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
 
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/workouts" element={<WorkoutsPage />} />
-            <Route path="/create-workout" element={<WorkoutFormPage />} />
-            <Route path="/progress" element={<ProgressPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/workouts" element={<WorkoutsPage />} />
+                <Route path="/create-workout" element={<WorkoutFormPage />} />
+                <Route path="/progress" element={<ProgressPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </WorkoutProvider>
+      </ExerciseProvider>
     </AuthProvider>
   );
 }
