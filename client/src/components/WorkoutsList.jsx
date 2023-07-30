0c4045732e2241a1768b7b06b363624657b8e8f6
Diego Calvo <100451091@alumnos.uc3m.es>
2023-07-31 00:00:16 +0200

    refactor (workouts page): extract workouts list into a new component

---
+++ client/src/components/WorkoutsList.jsx
@@ -0,0 +1,23 @@
+import Loading from "./Loading";
+import { useWorkoutContext } from "../context/WorkoutContext";
+import WorkoutCard from "./cards/WorkoutCard";
+
+const WorkoutsList = () => {
+  const { workouts, isLoading } = useWorkoutContext();
+
+  if (isLoading) return <Loading />;
+
+  if (workouts.length === 0) return (
+    <p>No workouts created yet</p>
+  )
+
+  return (
+    <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+      {workouts.map((workout, index) => (
+        <WorkoutCard key={index} workout={workout} />
+      ))}
+    </ul>
+  );
+};
+
+export default WorkoutsList;
--- client/src/pages/WorkoutsPage.jsx
+++ client/src/pages/WorkoutsPage.jsx
@@ -1,25 +1,13 @@
 import Header from "../components/Header";
-import Loading from "../components/Loading";
-import { useWorkoutContext } from "../context/WorkoutContext";
-import WorkoutCard from '../components/WorkoutCard';
+import WorkoutsList from '../components/WorkoutsList';
 
 function WorkoutsPage() {
-  const { workouts, isLoading } = useWorkoutContext();
-
   return (
     <>
       <Header title="Workouts" />
       <main className="mt-12 py-4 px-4 flex flex-col gap-3 bg-white lg:grid lg:px-8">
         <h3 className="text-lg font-bold">Created workouts</h3>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-            {workouts.map((workout, index) => (
-              <WorkoutCard key={index} workout={workout} />
-            ))}
-          </ul>
-        )}
+        <WorkoutsList />
       </main>
     </>
   );
