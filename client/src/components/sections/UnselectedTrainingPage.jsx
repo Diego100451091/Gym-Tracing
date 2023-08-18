80ca2f4c3884bd76ac2105092c81111e3f2ac8b5
Diego Calvo <100451091@alumnos.uc3m.es>
2023-08-18 23:38:27 +0200

    feat(new training page): implemented page for select a workout in new training page

---
+++ client/src/components/sections/UnselectedTrainingPage.jsx
@@ -0,0 +1,38 @@
+import { useWorkoutContext } from "../../context/WorkoutContext";
+import ActionButton from "../ActionButton";
+import { useNavigate } from "react-router-dom";
+
+const UnselectedTrainingPage = () => {
+  const { isLoading, workouts } = useWorkoutContext();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h3 className="text-lg font-bold">Workout selector</h3>
+      <p> Please, select one workout:</p>
+      {!isLoading ? (
+        <ul className="w-full flex flex-wrap justify-between items-center gap-3">
+          {workouts.map((workout) => (
+            <li
+              key={workout._id}
+              className="flex-grow flex flex-col items-center xs:grid grid-cols-6 place-items-center gap-2 rounded-md shadow-sm p-2 border border-spacing-0 border-gray-300"
+            >
+              <span className="w-full flex items-center col-span-4 text-sm md:text-base overflow-hidden">
+                {workout?.name}
+              </span>
+              <ActionButton
+                onClick={() => navigate(`/new-training/${workout._id}`)}
+                size="small"
+                customClass={"col-span-2 xs:place-self-end"}
+              >
+                Start
+              </ActionButton>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
+};
+
+export default UnselectedTrainingPage;
