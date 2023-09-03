48bc5afc2723cf5290e20e04ed73049c49ca5cea
Diego Calvo <100451091@alumnos.uc3m.es>
2023-09-03 23:11:24 +0200

    feat (workout record card hook): implemented custom hook to manage the logic of the workout record exercise cards

--- client/src/hooks/workout.hooks.js
+++ client/src/hooks/workout.hooks.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { requestWorkout } from "../api/workout.api";
 import { useNavigate } from "react-router-dom";
 import { throwErrorWithCallback } from "../alerts/AlertProvider";
+import { useExerciseInformation } from "./exercise.hooks";
+import { useWorkoutRecordContext } from "../context/WorkoutRecordContext";
 
 export const useWorkoutInformation = (workoutId) => {
   const [workout, setWorkout] = useState(null);
@@ -29,5 +31,44 @@ export const useWorkoutInformation = (workoutId) => {
   return {
     workout,
     isLoading,
+  };
+};
+
+export const useWorkoutRecordExerciseCard = (exerciseId) => {
+  const { exercise: exerciseInfo, isLoading: isExerciseInfoLoading } =
+    useExerciseInformation(exerciseId);
+  const { exercises, skippedExercises } = useWorkoutRecordContext();
+  let exerciseRecord = exercises
+    ? exercises.find((exercise) => exercise.id == exerciseId)
+    : null;
+  let isCardLoading = isExerciseInfoLoading || !exerciseInfo;
+  const isSkipped = skippedExercises[exerciseId] ?? false;
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleIsExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const isFullfilled = useMemo(() => {
+    if (exerciseRecord) {
+      const isWarmUpFullfilled =
+        exerciseRecord.warmUp.reps !== "" &&
+        exerciseRecord.warmUp.weight !== "";
+      const areSetsFullfilled = exerciseRecord.sets.every(
+        (set) => set.reps !== "" && set.weight !== ""
+      );
+      return isWarmUpFullfilled && areSetsFullfilled;
+    }
+    return false;
+  }, [exercises]);
+
+  return {
+    exerciseInfo,
+    exerciseRecord,
+    isCardLoading,
+    isFullfilled,
+    isSkipped,
+    isExpanded,
+    toggleIsExpanded,
   };
 };
